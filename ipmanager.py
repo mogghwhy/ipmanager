58d09c74ba86a112d38ba69a83f80d69c7f792f9
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-17 15:41:58 +0300

    read csv and costruct data dictionary

--- ipmanager.py
+++ ipmanager.py
@@ -1,9 +1,46 @@
 import csv
+from sys import argv
 
 fieldnames = ['source_ip', 'dest_port', 'count']
+key1 = fieldnames[0]
+key2 = fieldnames[1]
+valueKeys = ['count']
+updateKeys = ['addedDate', '']
 
 def readCsv(name, fieldnames):
+    dictList = None
     with open(name, newline='') as csvfile:
-        reader = csvfile.DictReader(csvfile, fieldnames=fieldnames)
+        reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+        dictList = list(reader)
+    return dictList[1:]
 
+def constructDataDict(dictlist, key1, key2, valueKeys):
+    data = {}
+    for item in dictlist:
+        level1key = item[key1]
+        level2key = item[key2]
+        dataLevel1 = data.get(level1key)
+        if dataLevel1 == None:
+            data[level1key] = {}
+            dataLevel1 = data[level1key]
+            dataLevel1[level2key] = {}
+            dataLevel2 = dataLevel1[level2key]
+            for vk in valueKeys:
+                dataLevel2[vk] = item[vk]
+        else:
+            dataLevel2 = dataLevel1.get(level2key)
+            if dataLevel2 == None:
+                dataLevel1[level2key] = {}
+                dataLevel2 = dataLevel1[level2key]
+                for vk in valueKeys:
+                    dataLevel2[vk] = item[vk]
+            else:
+                for vk in valueKeys:
+                    dataLevel2[vk] = item[vk]
+    return data
 
+if len(argv) == 2:
+    filename = argv[1]
+newitems = readCsv(name=filename, fieldnames=fieldnames)
+data = constructDataDict(newitems, key1=key1, key2=key2, valueKeys=valueKeys)
+print(data)
