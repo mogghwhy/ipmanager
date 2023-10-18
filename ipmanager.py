dd90a2550604a76447b5dbf53d9fa3436f63efa1
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-18 12:21:56 +0300

    variables renamed

--- ipmanager.py
+++ ipmanager.py
@@ -4,84 +4,84 @@ from sys import argv
 import datetime
 
 
-def readConfig(file):
+def read_config(file):
     with open(file, 'r', encoding='utf-8') as json_file:
         json_data = json.load(json_file)            
     return json_data    
 
 
-def readCsv(name, fieldnames):
-    dictList = None
+def read_csv(name, fieldnames):
+    dict_list = None
     with open(name, newline='') as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-        dictList = list(reader)
-    return dictList[1:]
+        dict_list = list(reader)
+    return dict_list[1:]
 
 
-def constructDataDict(dictlist, key1, key2, valueKeys):
+def construct_data_dict(dict_list, key1, key2, value_keys):
     data = {}
-    for item in dictlist:
-        level1key = item[key1]
-        level2key = item[key2]
-        dataLevel1 = data.get(level1key)
-        if dataLevel1 == None:
-            data[level1key] = {}
-            dataLevel1 = data[level1key]
-            dataLevel1[level2key] = {}
-            dataLevel2 = dataLevel1[level2key]
-            for vk in valueKeys:
-                dataLevel2[vk] = item[vk]
+    for item in dict_list:
+        level1_key = item[key1]
+        level2_key = item[key2]
+        data_level1 = data.get(level1_key)
+        if data_level1 == None:
+            data[level1_key] = {}
+            data_level1 = data[level1_key]
+            data_level1[level2_key] = {}
+            data_level2 = data_level1[level2_key]
+            for vk in value_keys:
+                data_level2[vk] = item[vk]
         else:
-            dataLevel2 = dataLevel1.get(level2key)
-            if dataLevel2 == None:
-                dataLevel1[level2key] = {}
-                dataLevel2 = dataLevel1[level2key]
-                for vk in valueKeys:
-                    dataLevel2[vk] = item[vk]
+            data_level2 = data_level1.get(level2_key)
+            if data_level2 == None:
+                data_level1[level2_key] = {}
+                data_level2 = data_level1[level2_key]
+                for vk in value_keys:
+                    data_level2[vk] = item[vk]
             else:
-                for vk in valueKeys:
-                    dataLevel2[vk] = item[vk]
+                for vk in value_keys:
+                    data_level2[vk] = item[vk]
     return data
 
 
-def writeCsv(name, dictList, fieldnames, key1, key2):
-    with open(name, 'w', newline='') as csvfile:        
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
+def write_csv(name, dict_list, field_names, key1, key2):
+    with open(name, 'w+', newline='') as csvfile:        
+        writer = csv.DictWriter(csvfile, fieldnames=field_names, dialect='excel')
         writer.writeheader()
-        for k1value in dictList:
-            level1 = dictList[k1value]
-            for k2value in level1:
-                level2 = level1[k2value]                
-                row = {key1: k1value, key2: k2value} | level2
+        for k1_value in dict_list:
+            level1 = dict_list[k1_value]
+            for k2_value in level1:
+                level2 = level1[k2_value]                
+                row = {key1: k1_value, key2: k2_value} | level2
                 writer.writerow(row) 
 
 
-def updateDataDict(sourceDataDict, updateDataDict, addKeys, updateKeys, compareKeys):
-    for source_key1 in sourceDataDict:
-        update_level1 = updateDataDict.get(source_key1)
+def update_data_dict(source_data_dict, update_data_dict, add_keys, update_keys, compare_keys):
+    for source_key1 in source_data_dict:
+        update_level1 = update_data_dict.get(source_key1)
         if update_level1 == None:
-            updateDataDict[source_key1] = sourceDataDict[source_key1]
-            update_level1 = updateDataDict[source_key1]
+            update_data_dict[source_key1] = source_data_dict[source_key1]
+            update_level1 = update_data_dict[source_key1]
             for k2 in update_level1:
-                for ak in addKeys:
+                for ak in add_keys:
                     update_level1[k2][ak] = datetime.datetime.now(tz=datetime.timezone.utc)
-                for uk in updateKeys:
+                for uk in update_keys:
                     update_level1[k2][uk] = datetime.datetime.now(tz=datetime.timezone.utc)
         else:
             pass
-            source_level1 = sourceDataDict[source_key1]
+            source_level1 = source_data_dict[source_key1]
             for source_key2 in source_level1:                
                 update_level2 = update_level1.get(source_key2)
                 if update_level2 == None:
                     update_level1[source_key2] = source_level1[source_key2]
                     update_level2 = update_level1[source_key2]                    
-                    for ak in addKeys:
+                    for ak in add_keys:
                         update_level2[ak] = datetime.datetime.now(tz=datetime.timezone.utc)
-                        for uk in updateKeys:
+                        for uk in update_keys:
                             update_level2[uk] = datetime.datetime.now(tz=datetime.timezone.utc)
                 else:
-                    for update_key2 in updateKeys:
-                        for compare_key in compareKeys:
+                    for update_key2 in update_keys:
+                        for compare_key in compare_keys:
                             update_value = update_level2[compare_key]
                             source_value = source_level1[source_key2][compare_key]
                             if source_value != update_value:
@@ -98,11 +98,11 @@ else:
     print("need to specify input and output files and their configs")
 
 
-input_config = readConfig(input_config)
-update_config = readConfig(update_config)
-input_items = readCsv(name=input_file, fieldnames=input_config['fieldnames'])
-input_data = constructDataDict(input_items, key1=input_config['key1'], key2=input_config['key2'], valueKeys=input_config['valueKeys'])
-update_items = readCsv(name=update_file, fieldnames=update_config['fieldnames'])
-update_data = constructDataDict(update_items, key1=update_config['key1'], key2=update_config['key2'], valueKeys=update_config['valueKeys'])
-updateDataDict(sourceDataDict=input_data, updateDataDict=update_data, addKeys=update_config['addKeys'], updateKeys=update_config['updateKeys'], compareKeys=update_config['compareKeys'])
-writeCsv(update_file, update_data, fieldnames=update_config['fieldnames'], key1=update_config['key1'], key2=update_config['key2'])
+input_config = read_config(input_config)
+update_config = read_config(update_config)
+input_items = read_csv(name=input_file, fieldnames=input_config['fieldnames'])
+input_data = construct_data_dict(input_items, key1=input_config['key1'], key2=input_config['key2'], value_keys=input_config['valueKeys'])
+update_items = read_csv(name=update_file, fieldnames=update_config['fieldnames'])
+update_data = construct_data_dict(update_items, key1=update_config['key1'], key2=update_config['key2'], value_keys=update_config['valueKeys'])
+update_data_dict(source_data_dict=input_data, update_data_dict=update_data, add_keys=update_config['addKeys'], update_keys=update_config['updateKeys'], compare_keys=update_config['compareKeys'])
+write_csv(update_file, update_data, field_names=update_config['fieldnames'], key1=update_config['key1'], key2=update_config['key2'])
